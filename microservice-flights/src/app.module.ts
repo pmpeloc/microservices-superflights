2d9ebb9c7926f433fc7692cb3dbe856d7e87cffc
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-11 22:23:12 -0300

    feat: Configuration for Microservice Flights added

--- microservice-flights/src/app.module.ts
+++ microservice-flights/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightModule } from './flight/flight.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FlightModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB),
+    FlightModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
