74a11e14986d18aefc5dc2f32049e7de8dd67410
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-11 21:17:38 -0300

    feat: Config microservice passengers added

--- microservice-passengers/src/app.module.ts
+++ microservice-passengers/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PassengerModule } from './passenger/passenger.module';
 
 @Module({
-  imports: [PassengerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB),
+    PassengerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
